921a7a04f1ea9a38e912dde1bba0c7489753c515
idrees <idrees@zomentum.com>
2024-02-05 17:33:29 +0530

    fix: height issues

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef, useCallback, useState } from 'react';
-import { useSession, signIn, getCsrfToken } from 'next-auth/react';
-import { SignInButton, StatusAPIResponse } from '@farcaster/auth-kit';
-import { useRouter } from 'next/router';
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from "../../styles/Common.module.css"
+import React, { useEffect, useRef, useCallback, useState } from "react";
+import { useSession, signIn, getCsrfToken } from "next-auth/react";
+import { SignInButton, StatusAPIResponse } from "@farcaster/auth-kit";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../../styles/Common.module.css";
 
 function SignIn() {
   const [error, setError] = useState(false);
@@ -30,9 +30,9 @@ function SignIn() {
   }, [isSignInComplete]);
 
   useEffect(() => {
-    if (status === 'authenticated' && !isSignInComplete) {
+    if (status === "authenticated" && !isSignInComplete) {
       setIsSignInComplete(true); // Mark sign-in as complete
-      router.replace('/room/host');
+      router.replace("/room/host");
     }
   }, [status, isSignInComplete, router]);
 
@@ -48,7 +48,7 @@ function SignIn() {
           videoRef.current.srcObject = stream;
         }
       } catch (err) {
-        console.error('Error accessing the webcam', err);
+        console.error("Error accessing the webcam", err);
       }
     };
 
@@ -58,27 +58,27 @@ function SignIn() {
     const handleRouteChange = () => {
       stopVideoStream();
     };
-    router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on("routeChangeStart", handleRouteChange);
 
     return () => {
       stopVideoStream();
-      router.events.off('routeChangeStart', handleRouteChange);
+      router.events.off("routeChangeStart", handleRouteChange);
     };
   }, [router.events]);
 
   const getNonce = useCallback(async () => {
     const nonce = await getCsrfToken();
-    if (!nonce) throw new Error('Unable to generate nonce');
-    console.log('Nonce: ', nonce);
+    if (!nonce) throw new Error("Unable to generate nonce");
+    console.log("Nonce: ", nonce);
     return nonce;
   }, []);
 
   const handleSuccess = useCallback(
     async (res: StatusAPIResponse) => {
-      console.log('Inside signin', res);
-      if (status !== 'authenticated') {
+      console.log("Inside signin", res);
+      if (status !== "authenticated") {
         // Only call signIn if not already authenticated
-        const result = await signIn('credentials', {
+        const result = await signIn("credentials", {
           message: res.message,
           signature: res.signature,
           name: res.username,
@@ -86,7 +86,7 @@ function SignIn() {
           redirect: false,
         });
         if (result?.ok) {
-          console.log('Sign in successful');
+          console.log("Sign in successful");
           console.log(JSON.stringify(result));
           setIsSignInComplete(true); // Mark sign-in as complete
 
@@ -97,13 +97,13 @@ function SignIn() {
     [status, router]
   );
 
-  if (status === 'authenticated' && isSignInComplete) {
+  if (status === "authenticated" && isSignInComplete) {
     // User is authenticated and sign-in is complete, no need to render SignInButton
     return null;
   }
 
   return (
-    <div className="flex flex-col w-full min-h-screen bg-white lg:m-0 lg:p-0">
+    <div className="flex flex-col w-full h-screen bg-white lg:m-0 lg:p-0">
       <div className="Nav mx-2 mt-4 mb-2 flex flex-row justify-between items-center bg-hero-bg py-2 rounded-2xl">
         <Image
           src="/logo.svg"
@@ -121,40 +121,70 @@ function SignIn() {
           />
         </div>
       </div>
-      <div className="ImageAndContent flex flex-col lg:flex-row lg:w-full mt-2 mx-3 mb-4">
-        <div className="w-full lg:w-1/2 lg:bg-slate-500 mr-2 mt-2 mb-4 rounded-3xl">
+      <div className="ImageAndContent flex flex-1 lg:flex-row lg:w-full mt-2">
+        <div className="w-full flex-1 lg:w-1/2 lg:bg-slate-500 mx-3 mt-2 mb-4 rounded-3xl">
           <video
             ref={videoRef}
             autoPlay
             muted
             className="lg:w-full lg:h-full rounded-3xl"
-            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
           />
         </div>
-        <div className="w-full lg:w-1/2 flex flex-col lg:ml-2 lg:mr-4 mt-2 mb-4 justify-center items-center bg-signin-content-bg h-full rounded-xl">
+        <div className="w-full lg:w-1/2 flex flex-col flex-1 lg:ml-2 lg:mr-4 mt-2 mb-4 justify-center items-center bg-signin-content-bg rounded-xl">
           <div className="lg:m-16 lg:p-8 relative">
-            <div className={`font-mona lg:text-3xl text-xl font-black uppercase text-black lg:mb-4 ${styles.signInSubTitle}`}>
+            <div
+              className={`font-mona lg:text-3xl text-xl font-black uppercase text-black lg:mb-4 ${styles.signInSubTitle}`}
+            >
               Welcome to Showcast
             </div>
-            <div className={`font-mona lg:pt-4 lg:pb-6 pt-5 pb-6 font-black uppercase text-hero-bg lg:text-6xl text-4xl ${styles.signInSubTitle}`}>
+            <div
+              className={`font-mona lg:pt-4 lg:pb-6 pt-5 pb-6 font-black uppercase text-hero-bg lg:text-6xl text-4xl ${styles.signInSubTitle}`}
+            >
               Ready to Meet
               <br />
               Farcaster Frens?
             </div>
             <hr className="opacity-30" />
             <div>
-              <p className="font-manrope font-thin"><span className="font-medium">1. Age Limit:</span> Must be 18+ or 13+ with parental consent</p>
-              <p className="font-manrope font-thin"><span className="font-medium">2. Content Alert:</span> May encounter adult or offensive content</p>
               <p className="font-manrope font-thin">
-                <span className="font-medium">3. Respect Others:</span> No harassment or discrimination tolerated
+                <span className="font-medium">1. Age Limit:</span> Must be 18+
+                or 13+ with parental consent
               </p>
-              <p className="font-manrope font-thin"><span className="font-medium">4. Protect Privacy:</span> Avoid sharing personal info</p>
-              <p className="font-manrope font-thin"><span className="font-medium">5. Use at Own Risk:</span> Not liable for user interactions</p>
+              <p className="font-manrope font-thin">
+                <span className="font-medium">2. Content Alert:</span> May
+                encounter adult or offensive content
+              </p>
+              <p className="font-manrope font-thin">
+                <span className="font-medium">3. Respect Others:</span> No
+                harassment or discrimination tolerated
+              </p>
+              <p className="font-manrope font-thin">
+                <span className="font-medium">4. Protect Privacy:</span> Avoid
+                sharing personal info
+              </p>
+              <p className="font-manrope font-thin">
+                <span className="font-medium">5. Use at Own Risk:</span> Not
+                liable for user interactions
+              </p>
             </div>
             <hr className="opacity-30" />
             <div className="font-manrope font-light text-gray-500">
-              Signing up for a Showcast account means you agree to the <Link className="no-underline outline-none text-black font-medium" href="#">Privacy
-              Policy</Link> and <Link className="no-underline outline-none text-black font-medium" href="#">Terms of Service</Link>.
+              Signing up for a Showcast account means you agree to the{" "}
+              <Link
+                className="no-underline outline-none text-black font-medium"
+                href="#"
+              >
+                Privacy Policy
+              </Link>{" "}
+              and{" "}
+              <Link
+                className="no-underline outline-none text-black font-medium"
+                href="#"
+              >
+                Terms of Service
+              </Link>
+              .
             </div>
             <div className="mt-4 px-10 py-4 bg-hero-bg flex flex-row justify-center text-white font-manrope rounded-xl">
               <SignInButton
